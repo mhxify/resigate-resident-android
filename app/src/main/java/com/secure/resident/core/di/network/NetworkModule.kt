b1be5be2a8e6574a5b8bda98f25bc53e3e275c8f
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-19 01:44:51 +0100

    network module di

---
+++ app/src/main/java/com/secure/resident/core/di/network/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.secure.resident.core.di.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            prettyPrint = false
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        json: Json
+    ): HttpClient {
+        return HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(json)
+            }
+        }
+    }
+}
