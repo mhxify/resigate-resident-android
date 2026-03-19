0dcecbb97752707a5fb309022c5c357daad881be
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-19 05:32:47 +0100

    result state

---
+++ app/src/main/java/com/secure/resident/core/presentation/state/ResultState.kt
@@ -0,0 +1,8 @@
+package com.secure.resident.core.presentation.state
+
+sealed class ResultState<out T> {
+    object Idle : ResultState<Nothing>()
+    object Loading : ResultState<Nothing>()
+    data class Success<T>(val data: T) : ResultState<T>()
+    data class Error(val message: String) : ResultState<Nothing>()
+}
