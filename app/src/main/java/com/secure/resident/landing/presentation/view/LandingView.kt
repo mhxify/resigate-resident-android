71a72344adbf36c194d5fa1fa47319f71b2993b1
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-19 22:29:26 +0100

    loggedIn navigation

--- app/src/main/java/com/secure/resident/landing/presentation/view/LandingView.kt
+++ app/src/main/java/com/secure/resident/landing/presentation/view/LandingView.kt
@@ -17,10 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.secure.resident.R
+import com.secure.resident.auth.data.local.AuthPrefs
+import com.secure.resident.auth.navigation.AuthAction
 import com.secure.resident.landing.navigation.LandingAction
 import kotlinx.coroutines.delay
 
@@ -29,10 +32,17 @@ fun LandingView(
     navController: NavController
 ){
     var big by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val isLoggedIn by remember { mutableStateOf(AuthPrefs.isLoggedIn(context)) }
+
     LaunchedEffect(Unit) {
         big = !big
         delay(1000)
-        LandingAction.navigationToOnBoardingView(navController)
+        if (isLoggedIn) {
+            AuthAction.navigationToMainFlow(navController)
+        }else {
+            LandingAction.navigationToOnBoardingView(navController)
+        }
     }
 
     val size by animateDpAsState(
