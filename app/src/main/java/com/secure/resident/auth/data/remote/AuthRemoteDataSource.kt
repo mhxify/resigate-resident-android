1e02d0066ad7f126057cc9b825dc9e1d69081e22
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-19 22:27:12 +0100

    verify otp , send otp , reset password api's

--- app/src/main/java/com/secure/resident/auth/data/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/com/secure/resident/auth/data/remote/AuthRemoteDataSource.kt
@@ -13,6 +13,7 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import io.ktor.http.isSuccess
 import javax.inject.Inject
 
 class AuthRemoteDataSource @Inject constructor(
@@ -52,6 +53,78 @@ class AuthRemoteDataSource @Inject constructor(
         println("RAW BODY = ${response.bodyAsText()}")
 
         return response.body()
+    }
+
+    suspend fun sendOtp(email: String) {
+        val response = httpClient.post("${baseUrl}auth/send") {
+            url {
+                parameters.append("email", email)
+            }
+            contentType(ContentType.Application.Json)
+        }
+
+        val rawBody = response.bodyAsText()
+
+        println("REMOTE STATUS = ${response.status}")
+        println("REMOTE RAW BODY = $rawBody")
+
+        if (!response.status.isSuccess()) {
+            println("REMOTE THROWING EXCEPTION NOW")
+            throw Exception("HTTP ${response.status.value}: $rawBody")
+        }
+
+        println("REMOTE SUCCESS")
+    }
+
+    suspend fun verifyOtp(
+        email: String ,
+        otp : String
+    ) {
+        val response = httpClient.post("${baseUrl}auth/verify") {
+            url {
+                parameters.append("email", email)
+                parameters.append("code", otp)
+            }
+            contentType(ContentType.Application.Json)
+        }
+
+        val rawBody = response.bodyAsText()
+
+        println("REMOTE STATUS = ${response.status}")
+        println("REMOTE RAW BODY = $rawBody")
+
+        if (!response.status.isSuccess()) {
+            println("REMOTE THROWING EXCEPTION NOW")
+            throw Exception("HTTP ${response.status.value}: $rawBody")
+        }
+
+        println("REMOTE SUCCESS")
+    }
+
+
+    suspend fun resetPassword(
+        email: String ,
+        password : String
+    ) {
+        val response = httpClient.post("${baseUrl}users/forgot-password") {
+            url {
+                parameters.append("email", email)
+                parameters.append("password", password)
+            }
+            contentType(ContentType.Application.Json)
+        }
+
+        val rawBody = response.bodyAsText()
+
+        println("REMOTE STATUS = ${response.status}")
+        println("REMOTE RAW BODY = $rawBody")
+
+        if (!response.status.isSuccess()) {
+            println("REMOTE THROWING EXCEPTION NOW")
+            throw Exception("HTTP ${response.status.value}: $rawBody")
+        }
+
+        println("REMOTE SUCCESS")
     }
 
 }
