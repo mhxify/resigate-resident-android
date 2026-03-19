f6b612cc9a8e1448ff4740cd99ec6178f4d2ae58
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-19 06:03:39 +0100

    user model

---
+++ app/src/main/java/com/secure/resident/auth/domain/model/User.kt
@@ -0,0 +1,12 @@
+package com.secure.resident.auth.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val email: String,
+    val fullname: String,
+    val imageUrl: String,
+    val role: String,
+    val userId: String
+)
