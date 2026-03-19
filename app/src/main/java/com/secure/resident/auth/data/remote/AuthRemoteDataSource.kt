f8eb4446b6551102f0d26e14b687195e989ed757
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-19 06:03:46 +0100

    get me api

--- app/src/main/java/com/secure/resident/auth/data/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/com/secure/resident/auth/data/remote/AuthRemoteDataSource.kt
@@ -1,11 +1,13 @@
 package com.secure.resident.auth.data.remote
 
-import android.app.Application
 import com.secure.resident.auth.data.model.login.request.LoginDtoRequest
 import com.secure.resident.auth.data.model.login.response.LoginDtoResponse
+import com.secure.resident.auth.domain.model.User
 import com.secure.resident.core.data.remote.baseUrl
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
@@ -38,6 +40,18 @@ class AuthRemoteDataSource @Inject constructor(
         return response.body()
     }
 
+    suspend fun getMe(
+        token : String
+    ) : User {
+        val response = httpClient.get("${baseUrl}auth/me") {
+            contentType(ContentType.Application.Json)
+            bearerAuth(token)
+        }
 
+        println("STATUS = ${response.status}")
+        println("RAW BODY = ${response.bodyAsText()}")
+
+        return response.body()
+    }
 
 }
