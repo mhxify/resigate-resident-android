401d692fb9cb1141500bba609b4849e18ae0317e
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-03-23 01:16:35 +0100

    get groups linking

--- app/src/main/java/com/secure/resident/main/presentation/view/section/chat/ChatView.kt
+++ app/src/main/java/com/secure/resident/main/presentation/view/section/chat/ChatView.kt
@@ -1,30 +1,135 @@
 package com.secure.resident.main.presentation.view.section.chat
 
-import androidx.compose.foundation.Image
+import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
+import com.secure.resident.auth.data.local.AuthPrefs
+import com.secure.resident.core.data.remote.baseUrl
+import com.secure.resident.core.presentation.component.CircularIndicator
 import com.secure.resident.core.presentation.component.PrimaryText
+import com.secure.resident.core.presentation.state.ResultState
+import com.secure.resident.main.data.local.ChatPrefs
+import com.secure.resident.main.navigation.MainAction
+import com.secure.resident.main.presentation.viewmodel.chat.getUserGroup.GetUserGroupViewModel
 
 @Composable
 fun ChatScreen(
-    navController: NavController
+    navController: NavController ,
+    getUserGroupViewModel: GetUserGroupViewModel = hiltViewModel()
 ) {
 
+    val context = LocalContext.current
+    val userId = remember { AuthPrefs.getUser(context).userId }
+    val token = remember { AuthPrefs.getToken(context) }
+    val userGroupState by getUserGroupViewModel.getGroupsState.collectAsState()
+
+    LaunchedEffect(userId, token) {
+        if (!userId.isNullOrBlank() && !token.isNullOrBlank()) {
+            getUserGroupViewModel.getUserGroups(userId, token)
+        }
+    }
+
+    val errorMessage = (userGroupState as? ResultState.Error)?.message
+
+    LaunchedEffect(errorMessage) {
+        if (!errorMessage.isNullOrBlank()) {
+            Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+            getUserGroupViewModel.resetGroupState()
+        }
+    }
+
+    when (val state = userGroupState) {
+        is ResultState.Loading -> {
+            CircularIndicator()
+        }
+
+        is ResultState.Success -> {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp) ,
+                modifier = Modifier.fillMaxSize()
+            ) {
+
+                PrimaryText(
+                    "Messages" ,
+                    needBold = true
+                )
+
+                if (state.data.isNotEmpty()) {
+                    LazyColumn(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        items(
+                            items = state.data,
+                            key = { group -> group.groupId?:"" }
+                        ) { group ->
+                            val fullImageUrl =
+                                "${baseUrl.removeSuffix("/")}${group.imageUrl}"
+
+                            ChatView(
+                                imageUrl = fullImageUrl,
+                                groupName = group.groupName ?:"",
+                                onClick = {
+
+                                    ChatPrefs.setGroupInfo(
+                                        groupId = group.groupId ?:"",
+                                        groupName = group.groupName ?:"",
+                                        groupImage = fullImageUrl ,
+                                        groupCreatedAt = group.createdAt ?:"",
+                                        context = context
+                                    )
+
+                                    MainAction.navigationToGroupChat(navController)
+
+                                }
+                            )
+                        }
+                    }
+                } else {
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        PrimaryText(
+                            text = "You have no groups at the moment",
+                            needBold = true
+                        )
+                    }
+                }
+            }
+        }
+        else -> Unit
+    }
 }
 
 @Composable
